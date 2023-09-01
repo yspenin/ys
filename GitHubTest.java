839b01567df4a588dc5d14eadb317426c9c773aa
yspenin <yuseong14@gmail.com>
2023-09-01 19:21:58 +0900

    'Add'

---
+++ GitHubTest.java
@@ -0,0 +1,11 @@
+
+public class GitHubTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 100;
+		float b = (int)((5/9f*(a-32))*100+1)/100f;
+		System.out.println(b); 
+	}
+
+}
